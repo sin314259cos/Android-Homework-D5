bf50495a5c5a8a61b39830c036febf927e544219
sin314259cos <763493572@qq.com>
2021-07-16 21:32:48 +0800

    TEST

---
+++ homework/app/src/main/java/com/bytedance/practice5/socket/SocketActivity.java
@@ -0,0 +1,47 @@
+package com.bytedance.practice5.socket;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bytedance.practice5.R;
+
+public class SocketActivity extends AppCompatActivity {
+    private TextView textView;
+    private static final String TAG = "chapter5";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socket);
+        textView = findViewById(R.id.tv_response);
+        SocketCallback callback =  new SocketCallback() {
+            @Override
+            public void onResponse(String s) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(s);
+                    }
+                });
+            }
+        };
+        findViewById(R.id.btn_send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"t1");
+                new ClientSocketThread(callback).start();
+            }
+        });
+    }
+
+    public static interface SocketCallback{
+        void onResponse(String s);
+    }
+}
